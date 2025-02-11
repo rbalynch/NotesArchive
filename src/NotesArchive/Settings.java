694cba629ec2147a738a0b0c4db616d06c7b4745
rbalynch <rbalynch@hotmail.com>
2025-02-11 11:56:19 -0600

    Settings layout changed to GridLayout, and each option has been changed to use a GridBagLayout. All components added, very close to flush.

--- src/NotesArchive/Settings.java
+++ src/NotesArchive/Settings.java
@@ -7,9 +7,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 public class Settings extends JDialog {
     static JPanel comps, factoryReset, autoIndexTime, lockdown;
@@ -18,36 +21,100 @@ public class Settings extends JDialog {
     static JComboBox<String> auDropdown;
     static JLabel frLabel, auLabel, ldLabel, frTitle, auTitle, ldTitle;
 
+    final static boolean shouldFill = true;
+    final static boolean shouldWeightX = true;
+
     public Settings() {
 
     }
 
     public static void addComponentsToFrame(Container pane) {
         comps = new JPanel(new GridLayout(0, 1));
+        Font font = new Font("Century Gothic", Font.BOLD, 16);
 
-        factoryReset = new JPanel();
-        frButton = new JButton("Reset");
+        factoryReset = new JPanel(new GridBagLayout());
+        GridBagConstraints c1 = new GridBagConstraints();
+        if (shouldFill) {
+            c1.fill = GridBagConstraints.HORIZONTAL;
+        }
+
+        if (shouldWeightX) {
+            c1.weightx = 0.5;
+        }
+
         frLabel = new JLabel("Reset the index, deleting all stored information on all text files");
+        c1.insets = new Insets(0, 10, 0, 4);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 1;
+        c1.gridy = 0;
+        factoryReset.add(frLabel, c1);
+
+        frButton = new JButton("Reset");
+        c1.weightx = 0.5;
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 2;
+        factoryReset.add(frButton, c1);
+
         frTitle = new JLabel("Factory Reset");
-        factoryReset.add(frTitle, BorderLayout.LINE_START);
-        factoryReset.add(frLabel, BorderLayout.CENTER);
-        factoryReset.add(frButton, BorderLayout.LINE_END);
+        frTitle.setFont(font);
+        c1.fill = GridBagConstraints.HORIZONTAL;
+        c1.gridx = 0;
+        factoryReset.add(frTitle, c1);
 
-        autoIndexTime = new JPanel();
-        auDropdown = new JComboBox<>(new String[]{"30 minutes", "60 minutes", "2 hours", "6 hours", "12 hours", "24 hours"});
+        autoIndexTime = new JPanel(new GridBagLayout());
+        GridBagConstraints c2 = new GridBagConstraints();
+        if (shouldFill) {
+            c2.fill = GridBagConstraints.HORIZONTAL;
+        }
+
+        if (shouldWeightX) {
+            c2.weightx = 0.5;
+        }
+
         auLabel = new JLabel("Set the program to automatically re-index all stored files to every...");
-        auTitle = new JLabel("Auto-Index Time");
-        autoIndexTime.add(auTitle, BorderLayout.LINE_START);
-        autoIndexTime.add(auLabel, BorderLayout.CENTER);
-        autoIndexTime.add(auDropdown, BorderLayout.LINE_END);
+        c2.gridx = 1;
+        c2.gridy = 0;
+        autoIndexTime.add(auLabel, c2);
 
-        lockdown = new JPanel();
-        ldField = new JTextField(10);
-        ldLabel = new JLabel("Set a password to your personal index.");
+        auDropdown = new JComboBox<>(new String[]{"30 minutes", "60 minutes", "2 hours", "6 hours", "12 hours", "24 hours"});
+        c2.fill = GridBagConstraints.HORIZONTAL;
+        c2.insets = new Insets(0, 10, 0, 5);
+        c2.gridx = 2;
+        autoIndexTime.add(auDropdown, c2);
+
+        auTitle = new JLabel("Auto-Index Time");
+        auTitle.setFont(font);
+        c2.fill = GridBagConstraints.HORIZONTAL;
+        c2.insets = new Insets(0, 10, 0, 18);
+        c2.gridx = 0;
+        autoIndexTime.add(auTitle, c2);
+
+        lockdown = new JPanel(new GridBagLayout());
+        GridBagConstraints c3 = new GridBagConstraints();
+        if (shouldFill) {
+            c3.fill = GridBagConstraints.HORIZONTAL;
+        }
+
+        if (shouldWeightX) {
+            c3.weightx = 0.5;
+        }
         ldTitle = new JLabel("Lockdown");
-        lockdown.add(ldTitle, BorderLayout.LINE_START);
-        lockdown.add(ldLabel, BorderLayout.CENTER);
-        lockdown.add(ldField, BorderLayout.LINE_END);
+        ldTitle.setFont(font);
+        c3.fill = GridBagConstraints.HORIZONTAL;
+        c3.insets = new Insets(0, 10, 0, 20);
+        c3.gridx = 0;
+        c3.gridy = 0;
+        lockdown.add(ldTitle, c3);
+
+        ldLabel = new JLabel("Set a password to your personal index.");
+        c3.gridx = 1;
+        c3.insets = new Insets(0, 0, 0, 30);
+        lockdown.add(ldLabel, c3);
+
+        ldField = new JTextField(10);
+        c3.gridx = 2;
+        c3.insets = new Insets(0, 30, 0, 5);
+        lockdown.add(ldField, c3);
 
         comps.add(factoryReset);
         comps.add(autoIndexTime);
