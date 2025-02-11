3321109ea753d478f60178906537373c12d2e37e
rbalynch <rbalynch@hotmail.com>
2025-02-11 14:24:19 -0600

    Fixed ResetPopup layout.

--- src/NotesArchive/ResetPopup.java
+++ src/NotesArchive/ResetPopup.java
@@ -6,19 +6,20 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.FlowLayout;
 
 public class ResetPopup {
     static JLabel warning, cont;
     static JButton yes, no;
-    static JPanel comps, buttons, text;
+    static JPanel buttons, text;
     static JFrame frame;
 
     public static void addComponentsToFrame(Container pane) {
-        text = new JPanel();
+        text = new JPanel(new FlowLayout());
         warning = new JLabel("This will delete the entire index. Any files indexed will not be affected.");
         cont = new JLabel("Continue?");
-        text.add(warning);
-        text.add(cont);
+        text.add(warning, BorderLayout.CENTER);
+        text.add(cont, BorderLayout.SOUTH);
 
         yes = new JButton("Confirm");
         no = new JButton("Cancel");
@@ -26,10 +27,8 @@ public class ResetPopup {
         buttons.add(yes);
         buttons.add(no);
 
-        comps = new JPanel();
-        comps.add(text, BorderLayout.PAGE_START);
-        comps.add(buttons, BorderLayout.PAGE_END);
-        pane.add(comps);
+        pane.add(text, BorderLayout.CENTER);
+        pane.add(buttons, BorderLayout.SOUTH);
     }
 
     public static void createGUI() {
